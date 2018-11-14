83f47f1a6f4bda9c445db209db6cbfcc9c00c133
majorazero <daniel.cw.hsu@gmail.com>
2018-11-14 11:54:57 -0800

    added some additional dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Landing from "./components/Landing/Landing.js";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>This is my portfolio.</div>
+      <div>
+        This is my portfolio.
+        <Landing></Landing>
+      </div>
     )
   }
 }
---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return(
+    <div>
+      Hello I'm the landing page.
+    </div>
+  );
+};
+
+export default Landing;
